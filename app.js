04815cab9c0d5360bc449b0eb3876768b6d36f11
Stanislav Minko <minkostas@gmail.com>
2023-01-26 15:12:58 +0300

    refactor: guess-2-way

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 /* let a = +prompt("введите число от 1 до 100"); */
 
-let a = Math.floor(Math.random()*1000);
+let a = Math.floor(Math.random()*100);
 console.log(a);
 
 function guessEnumeration(x){
-    for (let i = 0; i <= 1000; i++) {
-        const element = i;
+    for (let i = 0; i <= 100; i++) {
         if(x==i){
-            console.log(`BINGO! Вы загадали число${i}`);
+            console.log(`BINGO! Вы загадали число ${i}`);
             return i;
         }
     }
@@ -15,3 +14,29 @@ function guessEnumeration(x){
 
 let bingo = guessEnumeration(a);
 console.log(bingo);
+/* let x = 73; */
+
+function guess(x) {
+    let min = 0;
+    let max = 100;
+    let i;
+    for (i = min; i <= max; i++) {
+
+        console.log(`min:${min} and max:${max} `);
+        if(x==i){
+            console.log(`BINGO!BINGO! Вы загадали число ${i}`);
+            return i;
+        }
+        if(x>Math.floor(max/2)){
+            min = Math.floor(max/2);
+
+        } else{
+            max = Math.floor(max/2);
+
+        }
+    }
+}
+let r = 28;
+let rez = guess(r);
+
+console.log(rez);
